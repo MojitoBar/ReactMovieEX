38429eb36e8014fef1a2fb281a90a33d2a013b38
MojitoBar <wnehdtjr5@naver.com>
2018-06-27 23:12:36 +0900

    state 심화 학습

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies=[
-  {
-    title: "matrix",
-    poster: "https://i.pinimg.com/originals/93/ee/87/93ee871fa099e39a85b7d8f90f9512b0.jpg"
-  },
-  {
-    title: "full metar jacket",
-    poster: "http://fr.web.img5.acsta.net/medias/nmedia/18/65/57/12/19254508.jpg",
-  },
-  {
-    title: "old boy",
-    poster: "https://i.pinimg.com/originals/00/f2/51/00f251b177f602535ba5d6ac067a747f.jpg"
-  }
-]
+
 
 class App extends Component {
 
@@ -23,7 +10,21 @@ class App extends Component {
   // Update: componentWillRecevieProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: 'hello'
+    greeting: 'hello',
+    movies: [
+      {
+        title: "matrix",
+        poster: "https://i.pinimg.com/originals/93/ee/87/93ee871fa099e39a85b7d8f90f9512b0.jpg"
+      },
+      {
+        title: "full metar jacket",
+        poster: "http://fr.web.img5.acsta.net/medias/nmedia/18/65/57/12/19254508.jpg",
+      },
+      {
+        title: "old boy",
+        poster: "https://i.pinimg.com/originals/00/f2/51/00f251b177f602535ba5d6ac067a747f.jpg"
+      }
+    ]
   }
 
   componentDidMount() {
@@ -37,8 +38,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>
