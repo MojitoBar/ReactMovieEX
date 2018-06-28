b5d1629bbe4c743151b99efe06fd64fe7acaece3
MojitoBar <wnehdtjr5@naver.com>
2018-06-28 16:39:54 +0900

    Updating Movie:Component

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './Movie.css';
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis, rating}){
 	return (
-		<div>
-			<MoviePoster poster={poster}/>
-			<h1>{title}</h1>
+		<div className="Movie">
+			<div className="Movie__Columns">
+				<MoviePoster poster={poster} alt={title}/>
+			</div>
+			<div className="Movie__Columns">
+				<h1>{title}</h1>
+				<p className="Movie__rating">
+					{rating}
+				</p>
+				
+				<div className="Movie__Genres">
+					{genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+				</div>
+				<p className="Movie__Synopsis">
+					{synopsis}
+				</p>
+			</div>
 		</div>
 	)
 }
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
 	return (
-		<img src={poster} alt="Movie Poster"/>
+		<img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+	)
+}
+
+function MovieGenre({genre}){
+	return(
+		<span className="MOvie__Genre">{genre}  </span>
 	)
 }
 
