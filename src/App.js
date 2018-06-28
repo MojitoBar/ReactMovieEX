081d983be7eafbfbfb83e1849d004cf00d9ef7a7
MojitoBar <wnehdtjr5@naver.com>
2018-06-28 15:44:30 +0900

    AJAX on react

--- src/App.js
+++ src/App.js
@@ -9,34 +9,11 @@ class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillRecevieProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-    greeting: 'hello',
-    
-  }
+
+  state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-          movies: [
-            {
-              title: "matrix",
-              poster: "https://i.pinimg.com/originals/93/ee/87/93ee871fa099e39a85b7d8f90f9512b0.jpg"
-            },
-            {
-              title: "full metar jacket",
-              poster: "http://fr.web.img5.acsta.net/medias/nmedia/18/65/57/12/19254508.jpg",
-            },
-            {
-              title: "old boy",
-              poster: "https://i.pinimg.com/originals/00/f2/51/00f251b177f602535ba5d6ac067a747f.jpg"
-            },
-            {
-              title: "trainspotting",
-              poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcHoWC2d7UF6xrRP5dlloaB_Hmbn5uvfNkJVBCAzgGK-h7b4Rn"
-            }
-          ]
-      })
-    }, 5000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
   }
 
   _renderMovies = () => {
